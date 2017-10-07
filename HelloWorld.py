19c5ee0e3bb36e8969c0c77b36381f99741a3e89
rajisarma <rajisarma1995@yahoo.com>
2017-10-07 15:34:23 -0400

    functional fronted

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,9 +1,10 @@
 from bottle import route, run, request
+from collections import OrderedDict
+
 dict = {}
 
 @route('/')
 def hello():
-	#s = '<b><i>Hello World</b></i> <br> <br>'
     f ='''
         <form action = "/search" method = "get">
         <input name = "keywords" type = "text">
@@ -12,26 +13,39 @@ def hello():
         </b> <br>
         </form>
         '''
-    history = ['<table id = "History">']
-    for k in sorted(dict,key=dict.get,reverse=True):
-        history.append('<tr><td> %s </td>' % k)
-        history.append('<td> %d </td></tr>' % dict[k])
-    history.append('</table>')
-    return f, '\n'.join(history)
+    if bool(dict):
+        i=0
+        history = ['<table id = "history">']
+        history.append('<tr><td><b> Word </b></td>')
+        history.append('<td><b> Count </b></td></tr>')
+        for k in sorted(dict,key=dict.get,reverse=True):
+            if i<20:
+                history.append('<tr><td> %s </td>' % k)
+                history.append('<td> %d </td></tr>' % dict[k])
+                i += 1
+        history.append('</table>')
+        return f, '\n'.join(history)
+    else:
+        return f
 
 @route('/search', method='GET')
 def search():
     string = request.query['keywords']
     l = string.lower().split()
-    cur = {}
+    cur = OrderedDict()
     updateHistory(l,cur)
     updateHistory(l,dict)
-    results = ['<table id = "Results">']
-    for k in sorted(cur,key=cur.get,reverse=True):
-        results.append('<tr><td> %s </td>' % k)
-        results.append('<td> %d </td></tr>' % cur[k])
-    results.append('</table>')
-    return string, '\n'.join(results)
+    if len(cur)>1:
+        results = ['<table id = "results">']
+        results.append('<tr><td><b> Word </b></td>')
+        results.append('<td><b> Count </b></td></tr>')
+        for k in cur:
+            results.append('<tr><td> %s </td>' % k)
+            results.append('<td> %d </td></tr>' % cur[k])
+        results.append('</table>')
+        return string, '\n'.join(results)
+    else:
+        return string
 
 def updateHistory(l,dict):
     for i in l:
